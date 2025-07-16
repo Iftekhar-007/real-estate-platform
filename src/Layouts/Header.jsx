5b6f7b3b0b7a976ac2d60a5fb5ee0f2eb150ff58
Iftekhar-007 <shawonsp534@gmail.com>
2025-07-16 23:20:25 +0600

    okay

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -27,21 +27,13 @@ const Header = () => {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
             <li>
-              <a>Item 1</a>
+              <a>Home</a>
             </li>
             <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
+              <a>All Properties</a>
             </li>
             <li>
-              <a>Item 3</a>
+              <a>Dashboard</a>
             </li>
           </ul>
         </div>
@@ -50,23 +42,13 @@ const Header = () => {
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Item 1</a>
+            <a>Home</a>
           </li>
           <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
+            <a>All Properties</a>
           </li>
           <li>
-            <a>Item 3</a>
+            <a>Dashboard</a>
           </li>
         </ul>
       </div>
