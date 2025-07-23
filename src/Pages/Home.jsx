4c97c6965cf6d465a0e5c4b62134a7a0f8d8ffd2
Iftekhar-007 <shawonsp534@gmail.com>
2025-07-23 20:53:59 +0600

    okay

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Banner from "./Banner/Banner";
 import Articles from "../Layouts/Articles/Articles";
 import AgentCards from "../Layouts/AgentCard";
@@ -8,15 +8,6 @@ import AdvertisedProperties from "./AdvertisedProperties";
 // import LatestReviewCarousel from "../Layouts/LatestReviewCarousel";
 
 const Home = () => {
-  useEffect(() => {
-    document.title = "ZapEstate | Home";
-
-    const favicon = document.getElementById("favicon");
-    if (favicon) {
-      favicon.href = "../../public/real-estate.png";
-    }
-  }, []);
-
   return (
     <>
       <title>Home || Real estate</title>
